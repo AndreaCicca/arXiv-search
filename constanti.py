ab81e55b56dff72d7061bc1a4db742b92281a37e
Andrea Ciccarello <58073848+AndreaCicca@users.noreply.github.com>
2024-12-20 18:48:25 +0100

    aggiornamento delle costanti

--- constanti.py
+++ constanti.py
@@ -1,7 +1,14 @@
 ############# File che contiene le costanti #############
 
-PORT_DATABASE = 6333
-HOST_DATABASE = "localhost"
+# PORT_DATABASE = 6333
+# HOST_DATABASE = "localhost"
+# PDF_DIR = "arxiv_pdfs"
+# METADATA_DIR = "arxiv_metadata"
+# COLLECTION_NAME = "pdf_collection"
+
+
+PORT_DATABASE = 6555
+HOST_DATABASE = "192.168.1.26"
+COLLECTION_NAME = "Gruppo1"
 PDF_DIR = "arxiv_pdfs"
 METADATA_DIR = "arxiv_metadata"
-COLLECTION_NAME = "pdf_collection"
